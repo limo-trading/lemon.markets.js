662bad001f272ad4eb56d86f327f15b281931f7a
polycloud-dev <heidenreichdd@gmail.com>
2022-07-25 18:45:41 +0200

    added some tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -10,9 +10,33 @@ const client = new lemon.Client({
     trading_key: env.LEMON_API_KEY,
 });
 
-async function main() {
-    const positions = await client.positions.get();
-    console.log(positions.values);
+async function main(action: string) {
+    switch(action) {
+        case 'positions': displayPositions(); break;
+        case 'orders': order(); break;
+    }
 }
 
-main();
+async function order() {
+    const order = await client.orders.create({
+        isin: 'US0378331005',
+        quantity: 30,
+        expires_at: 'day',
+        side: 'buy',
+        venue: 'allday',
+    });
+    console.log(order);
+    const result = await order.activate({ pin: '1234' })
+    console.log(result);
+}
+
+async function displayPositions() {
+    const positions = await client.positions.get();
+    positions.values.forEach(async position => {
+        const quote = await client.quotes.latest({ isin: position.isin });
+        const avg_win = (quote.values[0].a - position.buy_price_avg/10000) * position.quantity;
+        console.log(`\x1b[36m${position.quantity}x\x1b[0m \x1b[44m${position.isin_title}\x1b[0m ${avg_win > 0 ? '\x1b[32m+' : '\x1b[31m'}${avg_win.toFixed(2)}\x1b[0m \x1b[35m${position.isin}\x1b[0m`);
+    })
+}
+
+main('positions');
