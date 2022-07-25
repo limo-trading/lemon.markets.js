f86e1600f197ba993ffe6603d7c563b50a67ac7a
polycloud-dev <heidenreichdd@gmail.com>
2022-07-25 15:29:54 +0200

    added dotenv

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,13 +1,18 @@
 import lemon from './index';
+import { load } from 'ts-dotenv';
+
+const env = load({
+    LEMON_API_KEY: String,
+});
 
 const client = new lemon.Client({
-    mode:'paper',
-    trading_key:'123',
+    mode: 'paper',
+    trading_key: env.LEMON_API_KEY,
 });
 
 async function main() {
     const positions = await client.positions.get();
-    console.log(positions);
+    console.log(positions.values);
 }
 
 main();
