95b2d9b440816d1e0dd6e938616821e5a25f592a
polycloud-dev <heidenreichdd@gmail.com>
2022-07-25 18:42:12 +0200

    added rate limits and post function

--- src/http_client.ts
+++ src/http_client.ts
@@ -1,19 +1,37 @@
 import fetch, { HeadersInit } from 'node-fetch';
-import { handleApiError } from './error';
+import LemonError, { handleApiError } from './error';
 
-type trading_mode = 'paper' | 'live';
+function awaitNewRateLimit(time: number) {
+    return new Promise<void>(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, time);
+    });
+}
 
 export default class HttpClient {
 
     private url: string;
     private auth_token: string;
 
-    constructor(mode: trading_mode, auth_token: string) {
-        this.url = `https://${mode === 'paper' ? 'paper-' : ''}trading.lemon.markets/v1`;
+    private rate_limit: number = 0;
+    private rate_remaining: number = 10;
+    private rate_reset: number = 0;
+
+    constructor(url: string, auth_token: string) {
+        this.url = url;
         this.auth_token = auth_token;
     }
 
     private async construct_fetch(url: string, method: 'GET' | 'POST', options: { headers?: HeadersInit, body?: string | object, query?: {[key: string]: string | number | undefined} }) {
+
+        // check rate limit
+        if(this.rate_remaining === 0) {
+            const time = this.rate_reset - new Date().getTime();
+            // if time is more than 5 seconds, return error
+            if(time > 5000) throw new LemonError('Rate limit exceeded. Try again in ~' + time / 1000 + ' seconds.');
+            await awaitNewRateLimit(time);
+        }
 
         const headers: HeadersInit = {
             'Authorization': `Bearer ${this.auth_token}`,
@@ -33,13 +51,24 @@ export default class HttpClient {
             body: method === 'POST' ? body : undefined
         });
         const data = await res.json();
-        if (res.status !== 200)
-            handleApiError(data);
+        if(res.status !== 200) handleApiError(data);
+
+        // update rate limit
+        this.rate_limit = parseInt(res.headers.get('RateLimit-Limit') || '0');
+        this.rate_remaining = parseInt(res.headers.get('RateLimit-Remaining') || '0');
+        // reset time
+        const seconds = parseInt(res.headers.get('RateLimit-Reset') || '0')
+        this.rate_reset = new Date().getTime() + seconds * 1000;
+
         return await data;
     }
     
     public get(path: string, query?: {[key: string]: string | number | undefined}) {
         return this.construct_fetch(`${this.url}${path}`, 'GET', { query });
+    }
+
+    public post(path: string, options: {}) {
+        return this.construct_fetch(`${this.url}${path}`, 'POST', options);
     }
 
     public external_fetch(url: string, options?: {}) {
