4ba851fc51bd870fa4207e980d4b110e880b0017
polycloud-dev <heidenreichdd@gmail.com>
2022-07-25 22:16:22 +0200

    added response type

--- src/clients/account.ts
+++ src/clients/account.ts
@@ -1,6 +1,22 @@
 import HttpClient from "../http_client";
 import ClientOptions from "./client_options";
 
+interface AccountResponse {
+    account_id: string
+    firstname: string
+    lastname: string
+    iban_brokerage: string
+    iban_origin: string
+    balance: bigint
+    cash_to_invest: bigint
+    cash_to_withdraw: bigint
+    amount_bought_intraday: bigint
+    amount_sold_intraday: bigint
+    amount_open_orders: bigint
+    amount_open_withdrawals: bigint
+    amount_estimate_taxes: bigint
+}
+
 export default class Account {
 
     private http_client: HttpClient;
@@ -10,6 +26,9 @@ export default class Account {
     }
 
     public async get() {
-        return await this.http_client.get('/account');
+        return new Promise<AccountResponse>(async resolve => {
+            const response = await this.http_client.get("/account");
+            resolve(response.results);
+        });
     }
 }
