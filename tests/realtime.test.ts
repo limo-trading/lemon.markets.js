c81b49c0cc28e6ae542e909e8ae9c98bea5e8b56
polycloud-dev <heidenreichdd@gmail.com>
2022-07-26 21:31:49 +0200

    added realtime test

---
+++ tests/realtime.test.ts
@@ -0,0 +1,39 @@
+import { RealtimeSubscription } from "../src/types";
+import client from "./client";
+
+describe("test realtime", () => {
+
+    let subscription: RealtimeSubscription
+
+    jest.setTimeout(10000)
+
+    it('should subscribe to realtime', () => {
+        return new Promise<void>(async resolve => {
+            const res = await client.realtime.subscribe({
+                isin: "US0378331005",
+                callback: (data) => { 
+
+                    expect(res).toHaveProperty('close')
+
+                    expect(data).toHaveProperty('isin')
+                    expect(data).toHaveProperty('b_v')
+                    expect(data).toHaveProperty('a_v')
+                    expect(data).toHaveProperty('b')
+                    expect(data).toHaveProperty('a')
+                    expect(data).toHaveProperty('t')
+                    expect(data).toHaveProperty('mic')
+
+                    res.close()
+
+                    resolve()
+                }
+            })
+
+            subscription = res
+        })
+    })
+
+    it('should unsubscribe from realtime', () => {
+        subscription.close()
+    })
+})
