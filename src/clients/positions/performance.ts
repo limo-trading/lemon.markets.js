3f3e02d7c28a5481ad0a5e961751d3d8292bfd58
polycloud-dev <heidenreichdd@gmail.com>
2022-07-26 12:56:09 +0200

    created performance route

---
+++ src/clients/positions/performance.ts
@@ -0,0 +1,37 @@
+import Client, { ClientOptions } from "../client";
+import ResponsePage, { toResponsePage } from "../response_page";
+
+interface PerformanceGetRequest {
+    isin?: string
+    from?: string
+    to?: string
+    sorting?: 'asc' | 'desc'
+    limit?: bigint
+    page?: bigint
+}
+
+interface PerformanceGetResponse {
+    isin: string
+    isin_title: string
+    profit: bigint
+    loss: bigint
+    quantity_bought: bigint
+    quantity_sold: bigint
+    quantity_open: bigint
+    opened_at: string
+    closed_at: string
+    fees: bigint
+}
+
+export default class Performance extends Client {
+
+    constructor(options: ClientOptions) {
+        super(options);
+    }
+    public async get(options?: PerformanceGetRequest) {
+        return new Promise<ResponsePage<PerformanceGetResponse>>(async resolve => {
+            const response = await this.http_client.get('/performance', { query: options });
+            resolve(toResponsePage(response, this.http_client));
+        });
+    }
+}
