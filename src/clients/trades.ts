a571fc9b1fea4279a00ed6926d866fe53dee9b29
polycloud-dev <heidenreichdd@gmail.com>
2022-07-26 20:41:38 +0200

    fixed small issues

--- src/clients/trades.ts
+++ src/clients/trades.ts
@@ -4,9 +4,9 @@ import { Trade } from "../types";
 
 interface TradesGetRequest {
     isin: string | string[]
-    mic: string
-    decimals: boolean
-    epoch: boolean
+    mic?: string
+    decimals?: boolean
+    epoch?: boolean
 }
 export default class Trades extends Client<void> {
 
@@ -18,7 +18,7 @@ export default class Trades extends Client<void> {
         return new Promise<ResponsePage<Trade>>(async resolve => {
             if(typeof options.isin !== 'string') options.isin = options.isin.join(',');
             const response = await this.httpClient.get('/trades/latest', { query: options });
-            resolve(new PageBuilder<Trade>(this.httpClient).build(response.results));
+            resolve(new PageBuilder<Trade>(this.httpClient).build(response));
         })
     }
 }
