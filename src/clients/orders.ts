7423a9852c6d23fa913854d902840f97ae3570d2
polycloud-dev <heidenreichdd@gmail.com>
2022-07-26 19:51:33 +0200

    extracted types and fixed small issues

--- src/clients/orders.ts
+++ src/clients/orders.ts
@@ -1,31 +1,13 @@
 import HttpClient from "../http_client";
 import Client, { ClientOptions } from "../client";
 import ResponsePage, { PageBuilder } from "../response_page";
-
-type OrderStatus = 'inactive' | 'active' | 'open' | 'in_progress' | 'canceling' | 'executed' | 'canceled' | 'expired'
+import { Order, OrderConfirmation, OrderStatus } from "../types";
 
 interface ActivateRequest {
     pin: number
 }
 
-interface Order {
-    created_at: string
-    id: string
-    status: string
-    isin: string
-    expires_at: string
-    side: 'buy' | 'sell'
-    quantity: number
-    stop_price: number
-    limit_price: number
-    venue: string
-    estimated_price: number
-    charge: number
-    activate: (options: { pin: string }) => Promise<boolean>
-}
-
 interface OrderGetRequest {
-    order_id?: string
     from?: string
     to?: string
     isin?: string
@@ -33,21 +15,6 @@ interface OrderGetRequest {
     status?: OrderStatus
 }
 
-interface OrderGetResponse {
-    id: string
-    isin: string
-    isin_title: string
-    expires_at: string
-    created_at: string
-    side: 'buy' | 'sell'
-    quantity: number
-    stop_price: number
-    limit_price: number
-    estimated_price: number
-    estimated_price_total: number
-    venue: string
-    status: OrderStatus
-}
 
 const activateFunction = async(options: ActivateRequest, id: string, httpClient: HttpClient) => {
     return new Promise<boolean>(async resolve => {
@@ -57,7 +24,7 @@ const activateFunction = async(options: ActivateRequest, id: string, httpClient:
     })
 }
 
-export default class Orders extends Client<OrderGetResponse> {
+export default class OrdersClient extends Client<Order> {
 
     constructor(options: ClientOptions) {
         super(options);
@@ -70,7 +37,7 @@ export default class Orders extends Client<OrderGetResponse> {
         side: 'buy' | 'sell'
         venue: string
     }) {
-        return new Promise<Order>(async resolve => {
+        return new Promise<OrderConfirmation>(async resolve => {
             const expiresAt = options.expires_at instanceof Date ? options.expires_at.toISOString() : 'p1d';
             const response = await this.httpClient.post('/orders', { body: { ...options, expiresAt } })
             resolve({
@@ -80,23 +47,22 @@ export default class Orders extends Client<OrderGetResponse> {
         })
     }
 
-    private getOne(orderId: string) {
-        return new Promise<OrderGetResponse>(async resolve => {
+    public getOne(orderId: string) {
+        return new Promise<Order>(async resolve => {
             const response = await this.httpClient.get(`/orders/${orderId}`)
-            this.cacheLayer.set(response.results[0].id, response.results)
+            this.cacheLayer.set(response.results.id, response.results)
             resolve({
                 activate: response.results.status === 'inactive' ? (options: ActivateRequest) => activateFunction(options, response.results.id, this.httpClient) : undefined,
-                ...response.results[0],
+                ...response.results,
             })
         })
     }
 
     public get(options?: OrderGetRequest) {
-        if(options?.order_id) return this.getOne(options.order_id);
-        return new Promise<ResponsePage<OrderGetResponse>>(async resolve => {
+        return new Promise<ResponsePage<Order>>(async resolve => {
             const response = await this.httpClient.get('/orders', { query: options })
             // add activate method to each inactive order
-            const orders = response.results.map((order: OrderGetResponse) => ({
+            const orders = response.results.map((order: Order) => ({
                 activate: order.status === 'inactive' ? (activateOptions: ActivateRequest) => activateFunction(activateOptions, order.id, this.httpClient) : undefined,
                 ...order,
             }))
