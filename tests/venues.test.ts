032be63d1fdb6ee5aed3b562c4b1bc28a30f6a41
polycloud-dev <heidenreichdd@gmail.com>
2022-07-26 20:28:49 +0200

    added venues test

---
+++ tests/venues.test.ts
@@ -0,0 +1,25 @@
+import { Venue } from "../src/types";
+import client from "./client";
+
+describe("test venues route", () => {
+
+    let venues: Venue[]
+
+    it("should get venues", async () => {
+        const res = await client.venues.get()
+        expect(res).toHaveProperty("values")
+        expect(res.values.length).toBeGreaterThan(0)
+
+        venues = res.values
+
+        expect(res.values[0]).toHaveProperty("name")
+        expect(res.values[0]).toHaveProperty("title")
+        expect(res.values[0]).toHaveProperty("mic")
+        expect(res.values[0]).toHaveProperty("is_open")
+    })
+
+    it("should be cached", async () => {
+        const cache = client.venues.cache()
+        expect(cache).toEqual(venues)
+    })
+})
