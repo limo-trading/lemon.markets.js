efb4ea04086ab3069b52b029573344b58a540199
polycloud-dev <heidenreichdd@gmail.com>
2022-07-26 17:09:17 +0200

    extracted bankstatement type

--- src/clients/account/bankstatements.ts
+++ src/clients/account/bankstatements.ts
@@ -1,7 +1,6 @@
 import Client, { ClientOptions } from "../../client";
 import ResponsePage, { PageBuilder } from "../../response_page";
-
-type BankStatementType = 'pay_in' | 'pay_out' | 'order_buy' | 'order_sell' | 'eod_balance' | 'dividend' | 'tax_refunded'
+import { BankStatement, BankStatementType } from "../../types";
 
 interface BankStatementsGetRequest {
     type?: BankStatementType
@@ -12,25 +11,14 @@ interface BankStatementsGetRequest {
     page?: number
 }
 
-interface BankStatementsGetResponse {
-    id: string
-    account_id: string
-    type: BankStatementType
-    date: string
-    amount: number
-    isin: string
-    isin_title: string
-    created_at: string
-}
-
-export default class BankStatements extends Client<BankStatementsGetResponse> {
+export default class BankStatements extends Client<BankStatement> {
     
     constructor(options: ClientOptions) {
         super(options);
     }
 
     public get(options?: BankStatementsGetRequest) {
-        return new Promise<ResponsePage<BankStatementsGetResponse>>(async resolve => {
+        return new Promise<ResponsePage<BankStatement>>(async resolve => {
             const response = await this.http_client.get('/account/bankstatements', { query: options });
             resolve(new PageBuilder(this.http_client, this.cache_layer).build(response));
         });
