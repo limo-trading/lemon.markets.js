0f36cea742968053283d7718dab0a688efc7b582
polycloud-dev <heidenreichdd@gmail.com>
2022-07-26 16:47:33 +0200

    test client

---
+++ tests/client.ts
@@ -0,0 +1,13 @@
+import lemon from '../src/index';
+import { load } from 'ts-dotenv'
+
+const env = load({
+    LEMON_API_KEY: String
+}, process.cwd() + '/tests/.env')
+
+const client = new lemon.Client({
+    mode: 'paper',
+    trading_key: env.LEMON_API_KEY,
+});
+
+export default client;
