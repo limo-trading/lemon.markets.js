f16ce21953527980d07c5ae189c114fc0a3263c2
polycloud-dev <heidenreichdd@gmail.com>
2022-07-26 13:07:28 +0200

    created document route

---
+++ src/clients/account/documents.ts
@@ -0,0 +1,26 @@
+import Client, { ClientOptions } from "../client";
+import ResponsePage, { toResponsePage } from "../response_page";
+
+interface DocumentsGetResponse {
+    id: string
+    name: string
+    created_at: string
+    category: string
+    link: string
+    viewed_first_at: string
+    viewed_last_at: string
+}
+
+export class Documents extends Client {
+    
+    constructor(options: ClientOptions) {
+        super(options);
+    }
+
+    public get() {
+        return new Promise<ResponsePage<DocumentsGetResponse>>(async resolve => {
+            const response = await this.http_client.get('/account/documents');
+            resolve(toResponsePage(response, this.http_client));
+        });
+    }
+}
