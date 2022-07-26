2e970aa1da24f13a64047bcfba8e7ce2f8b22471
polycloud-dev <heidenreichdd@gmail.com>
2022-07-26 16:47:42 +0200

    added account test

---
+++ tests/account.test.ts
@@ -0,0 +1,31 @@
+import { Account } from '../src/types/account';
+import client from './client'
+
+describe('test account route', () => {
+
+    let account: Account;
+
+    it('should return account', async () => {
+        const res = await client.account.get()
+        account = res
+        
+        expect(res).toHaveProperty('account_id')
+        expect(res).toHaveProperty('firstname')
+        expect(res).toHaveProperty('lastname')
+        expect(res).toHaveProperty('iban_brokerage')
+        expect(res).toHaveProperty('iban_origin')
+        expect(res).toHaveProperty('balance')
+        expect(res).toHaveProperty('cash_to_invest')
+        expect(res).toHaveProperty('cash_to_withdraw')
+        expect(res).toHaveProperty('amount_bought_intraday')
+        expect(res).toHaveProperty('amount_sold_intraday')
+        expect(res).toHaveProperty('amount_open_orders')
+        expect(res).toHaveProperty('amount_open_withdrawals')
+        expect(res).toHaveProperty('amount_estimate_taxes')
+    })
+
+    it('should be cached', async () => {
+        const cache = client.account.cache()
+        expect(cache).toEqual(account)
+    })
+})
