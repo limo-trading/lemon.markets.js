c192126040ea723569b623c39748a3e04e1753a9
polycloud-dev <heidenreichdd@gmail.com>
2022-07-26 20:24:23 +0200

    added instruments test

---
+++ tests/instruments.test.ts
@@ -0,0 +1,27 @@
+import { Instrument } from "../src/types";
+import client from "./client";
+
+describe("test instruments route", () => {
+
+    let instruments: Instrument[];
+
+    it("should get instruments aapl", async () => {
+        const res = await client.instruments.get({ isin: "US0378331005" })
+        expect(res).toHaveProperty("values")
+        expect(res.values.length).toBeGreaterThan(0)
+
+        instruments = res.values
+
+        expect(res.values[0]).toHaveProperty("isin")
+        expect(res.values[0]).toHaveProperty("wkn")
+        expect(res.values[0]).toHaveProperty("name")
+        expect(res.values[0]).toHaveProperty("title")
+        expect(res.values[0]).toHaveProperty("symbol")
+        expect(res.values[0]).toHaveProperty("type")
+    })
+
+    it("should be cached", async () => {
+        const cache = client.instruments.cache()
+        expect(cache).toEqual(instruments)
+    })
+})
