854a606a8d149a093f433827b3c2e0b2e0358984
polycloud-dev <heidenreichdd@gmail.com>
2022-07-26 12:51:36 +0200

    created statements route

---
+++ src/clients/positions/statements.ts
@@ -0,0 +1,36 @@
+import Client, { ClientOptions } from "../client";
+import ResponsePage, { toResponsePage } from "../response_page";
+
+type StatementType = 'order_buy' | 'order_sell' | 'split' | 'import' | 'snx'
+
+interface StatementGetRequest {
+    type?: StatementType
+    limit?: bigint
+    page?: bigint
+}
+
+interface StatementGetResponse {
+    id: string
+    order_id: string
+    external_id: string
+    type: StatementType
+    quantity: string
+    isin: string
+    isin_title: string
+    date: string
+    created_at: string
+}
+
+export default class Statements extends Client {
+    
+    constructor(options: ClientOptions) {
+        super(options);
+    }
+
+    public get(options?: StatementGetRequest) {
+        return new Promise<ResponsePage<StatementGetResponse>>(async resolve => {
+            const res = await this.http_client.get(`/statements`, { query: options })
+            resolve(toResponsePage(res, this.http_client))
+        })
+    }
+}
