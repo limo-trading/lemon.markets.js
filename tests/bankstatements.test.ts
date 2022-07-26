dc564242d346fbf3b55319149c99cdfb12bcbb35
polycloud-dev <heidenreichdd@gmail.com>
2022-07-26 17:13:53 +0200

    added bankstatements test

---
+++ tests/bankstatements.test.ts
@@ -0,0 +1,29 @@
+import { BankStatement } from '../src/types';
+import client from './client';
+
+describe('test bankstatements route', () => {
+    
+    let bankstatements: BankStatement[];
+
+    it('should get bankstatements', async () => {
+        const res = await client.account.bankstatements.get()
+        expect(res).toHaveProperty('values')
+        expect(res.values.length).toBeGreaterThan(0)
+
+        expect(res.values[0]).toHaveProperty('id')
+        expect(res.values[0]).toHaveProperty('account_id')
+        expect(res.values[0]).toHaveProperty('type')
+        expect(res.values[0]).toHaveProperty('date')
+        expect(res.values[0]).toHaveProperty('amount')
+        expect(res.values[0]).toHaveProperty('isin')
+        expect(res.values[0]).toHaveProperty('isin_title')
+        expect(res.values[0]).toHaveProperty('created_at')
+
+        bankstatements = res.values
+    })
+
+    it('should be cached', async () => {
+        const cache = client.account.bankstatements.cache()
+        expect(cache).toEqual(bankstatements)
+    })
+})
