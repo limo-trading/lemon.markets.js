154dbbc7736906c893f33586a6a2d1e12b1dc080
polycloud-dev <heidenreichdd@gmail.com>
2022-07-26 20:35:56 +0200

    support multiple isins

--- src/clients/quotes.ts
+++ src/clients/quotes.ts
@@ -2,14 +2,20 @@ import Client, { ClientOptions } from "../client";
 import ResponsePage, {PageBuilder } from "../response_page";
 import { Quote } from "../types";
 
+interface QuoteGetRequest {
+    isin: string | string[]
+    mic?: string
+}
+
 export default class Quotes extends Client<void> {
 
     constructor(options: ClientOptions) {
         super(options);
     }
 
-    public async latest(options: { isin: string, mic?: string }) {
+    public async latest(options: QuoteGetRequest) {
         return new Promise<ResponsePage<Quote>>(async resolve => {
+            if(typeof options.isin !== 'string') options.isin = options.isin.join(',') 
             const response = await this.httpClient.get('/quotes/latest', { query: options });
             resolve(new PageBuilder<Quote>(this.httpClient).build(response));
         })
