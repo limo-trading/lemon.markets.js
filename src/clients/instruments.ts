3b271ca7b59fe1db6d3cabb0c2fed8d8d057148d
polycloud-dev <heidenreichdd@gmail.com>
2022-07-28 14:56:52 +0200

    fixed small issue

--- src/clients/instruments.ts
+++ src/clients/instruments.ts
@@ -3,7 +3,7 @@ import { PageBuilder } from "../response_page";
 import { Instrument, InstrumentType, ResponsePage } from "../types";
 
 interface InstrumentsGetRequest {
-    isin?: string | string[10];
+    isin?: string | string[];
     search?: string;
     type?: InstrumentType;
 }
@@ -16,6 +16,7 @@ export default class InstrumentsClient extends Client<Instrument> {
 
     public get(options: InstrumentsGetRequest) {
         return new Promise<ResponsePage<Instrument>>(async resolve => {
+            if (options.isin && typeof options.isin !== 'string') options.isin = options.isin.join(',')
             const response = await this.httpClient.get('/instruments', { query: options });
             resolve(new PageBuilder<Instrument>(this.httpClient, this.cacheLayer).build(response));
         })
