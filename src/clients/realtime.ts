5c47f3ea1874e11d4ca7df3a80512e69953ab92c
polycloud-dev <heidenreichdd@gmail.com>
2022-08-01 13:09:13 +0200

    added browser support

--- src/clients/realtime.ts
+++ src/clients/realtime.ts
@@ -9,25 +9,24 @@ interface RealtimeSubscribeParams {
     callback: (data: Quote) => void
     allowOutOfOrder?: boolean
     decimals?: boolean
+    errorCallback?: (error: string) => void
 }
 
 export default class RealtimeClient extends Client<Quote> {
 
     private authCache: Cache<RealtimeAuth>;
     private connectionCache: Cache<Ably.Realtime>;
-    private quotesCache: Cache<Ably.Types.RealtimeChannelCallbacks>
-    private subscriptionsCache: Cache<Ably.Types.RealtimeChannelCallbacks>
+    private subscriptionsCache: Cache<RealtimeSubscription>
 
     constructor(options: ClientOptions) {
         super(options);
 
         this.authCache = new Cache();
         this.connectionCache = new Cache();
-        this.quotesCache = new Cache();
         this.subscriptionsCache = new Cache();
     }
 
-    private auth() {
+    private generateAuth() {
         return new Promise<RealtimeAuth>(async resolve => {
             const response = await this.httpClient.post('/auth');
             this.cacheLayer.set('auth', response);
@@ -38,23 +37,59 @@ export default class RealtimeClient extends Client<Quote> {
     public subscribe(options: RealtimeSubscribeParams) {
         return new Promise<RealtimeSubscription>(async (resolve, reject) => {
 
-            const decimals = options.decimals ?? true;
+            options.errorCallback = (error: string) => reject(error);
 
             const auth = this.authCache.getDefault()
                 ? this.authCache.getDefault().expires_at < Date.now()
-                    ? await this.auth() : this.authCache.getDefault() : await this.auth();
+                    ? await this.generateAuth() : this.authCache.getDefault() : await this.generateAuth();
 
             const connection = this.connectionCache.getDefault() || new Ably.Realtime({
                 token: auth.token,
                 transportParams: { remainPresentFor: 1000 },
                 authCallback: async (_, callback) => {
-                    const newAuth = await this.auth();
+                    const newAuth = await this.generateAuth();
                     callback('', newAuth.token);
                 }
             })
 
-            const quotes = this.quotesCache.getDefault() || connection.channels.get(auth.user_id);
-            const subscriptions = this.subscriptionsCache.getDefault() || connection.channels.get(`${auth.user_id}.subscriptions`);
+            if(!this.subscriptionsCache.getDefault()) this.subscriptionsCache.setDefault(await new Realtime(auth, connection).subscribe(options));
+            const subscription = this.subscriptionsCache.getDefault();
+
+            resolve(subscription);
+        })
+    }
+
+    public auth() {
+        const cache = this.authCache.getDefault();
+        if (cache && cache.expires_at > Date.now()) return cache;
+        return this.generateAuth();
+    }
+}
+
+export class Realtime {
+
+    private connection: Ably.Realtime;
+    private auth: RealtimeAuth;
+
+    private lastQuote: Quote | undefined;
+
+    constructor(auth: RealtimeAuth, connection?: Ably.Realtime) {
+        this.auth = auth;
+        this.connection = connection || new Ably.Realtime({
+            token: auth.token,
+            transportParams: { remainPresentFor: 1000 },
+            authCallback: async (_, callback) => {
+                callback('cannot generate new token', '');
+            }
+        })
+    }
+
+    public subscribe(options: RealtimeSubscribeParams) {
+        return new Promise<RealtimeSubscription>(async (resolve, reject) => {
+            const decimals = options.decimals ?? true;
+
+            const quotes = this.connection.channels.get(this.auth.user_id);
+            const subscriptions = this.connection.channels.get(`${this.auth.user_id}.subscriptions`);
 
             quotes.subscribe((message) => {
                 const { name, data } = message;
@@ -71,9 +106,8 @@ export default class RealtimeClient extends Client<Quote> {
                         if (options.allowOutOfOrder) return options.callback(quote);
 
                         // check if quote is newer than last quote
-                        const lastQuote = this.cacheLayer.get(quote.isin) || {};
-                        if (lastQuote.t?.getTime() < quote.t.getTime()) {
-                            this.cacheLayer.set(quote.isin, quote);
+                        if (!this.lastQuote || this.lastQuote.t?.getTime() < quote.t.getTime()) {
+                            this.lastQuote = quote;
                             options.callback(quote);
                         }
                         break;
@@ -83,7 +117,10 @@ export default class RealtimeClient extends Client<Quote> {
             })
 
             subscriptions.publish('isins', typeof options.isin === 'string' ? options.isin : options.isin.join(','), err => {
-                reject(err)
+                if(err) {
+                    if(!options.errorCallback) return console.log(err.message);
+                    return options.errorCallback?.(err.message);
+                }
             });
 
             resolve({
@@ -91,7 +128,7 @@ export default class RealtimeClient extends Client<Quote> {
                     return new Promise<void>(resolveClose => {
                         quotes.detach((err) => reject(err));
                         subscriptions.detach((err) => reject(err));
-                        connection.close();
+                        this.connection.close();
                         resolveClose()
                     })
                 }
